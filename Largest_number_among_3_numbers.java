c3a54b7e63791cb7d8601ff6539492bacfb6964c
patrunivarshini <119914370+patrunivarshini@users.noreply.github.com>
2024-02-17 21:17:56 +0530

    import java.util.Scanner; public class large{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int a=s.nextInt();         int b=s.nextInt();         int c=s.nextInt();         if(a>b && a>c){             System.out.println(a);         }         else if(b>c){             System.out.println(b);         }         else{             System.out.println(c);         }     } }

---
+++ Largest_number_among_3_numbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class large{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int c=s.nextInt();
+        if(a>b && a>c){
+            System.out.println(a);
+        }
+        else if(b>c){
+            System.out.println(b);
+        }
+        else{
+            System.out.println(c);
+        }
+    }
+}
